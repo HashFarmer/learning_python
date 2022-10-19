40bce888b93ccfd3bd4b66c816390c8f8cfa53d2
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-10-19 15:01:38 +0800

    Create class_method_1.py

---
+++ class_method_1.py
@@ -0,0 +1,20 @@
+
+# classmethod的真正用处不在于修改类变量，这一点staticmethod可以做到，实例函数也可以做到
+
+
+class Person:
+    age = 25
+
+    @classmethod
+    def printAge(cls):
+        print('The age is:', cls.age)
+
+# create printAge class method
+# Person.printAge = classmethod(Person.printAge) # 等价于@classmethod, 两者选其一
+
+# 1:
+# classmethod有两种调用方式
+Person.printAge()
+
+p1 = Person()
+p1.printAge()
