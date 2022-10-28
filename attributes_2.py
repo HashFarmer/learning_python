dfb9ea6643d72a8368f11ddc943335cd48814d19
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-10-28 12:35:37 +0800

    Update attributes_2.py

--- attributes_2.py
+++ attributes_2.py
@@ -15,12 +15,13 @@ if __name__ == '__main__':
     print(foo.__class__.class_attr)
 
 
+    
     #modify the class attribute as a foo property
     foo.class_attr = 5  # 问题出在这里，相当于为foo对象添加了名为class_attr的实例属性，碰巧与类属性同名而已
     print(foo.class_attr)
     #5
 
-
+    # 对“实例”来说，“类属性”只能被读，而不能被修改，一修改，就是实例属性了，而类属性依然不变
     #print the class attribute as a porperty of a bar
     print(bar.class_attr)  # 说明实例修改不了类属性的值
     # 0 
